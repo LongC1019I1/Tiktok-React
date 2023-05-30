254ccf825c4c424d7c713ccc0f7aeb147d698d96
LongC1019I1 <bachthanhlong.it@gmail.com>
2023-05-30 15:11:09 +0700

    router/ layout

---
+++ src/pages/Search/index.js
@@ -0,0 +1,17 @@
+
+
+function Search() {
+    return (
+        <div>
+            <div className="content">
+                <h2>Search page</h2>
+            </div>
+        </div>
+    )
+
+
+}
+
+
+
+export default Search;
